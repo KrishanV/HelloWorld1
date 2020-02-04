37490b9c4c81a7ffd1ceb96d48a759c26191cd08
KrishanV <tarpeyboy07@gmail.com>
2020-02-03 16:40:42 -0800

    Added 2 name variables

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,11 +1,11 @@
 class HelloWorld{
     
-    constructor(public name:string){
+    constructor(public Firstname:string, lastName:string){
 
     }
     greet(){
-        console.log("'Hello World!' My name is " + this.name + ".");
+        console.log("'Hello World!' My name is " + this.firstName + " " + this.lastName + ".");
     }
 }
-const helloWorld = new HelloWorld("Krishan Verma");
+const helloWorld = new HelloWorld("Krishan" + "Verma");
 helloWorld.greet();
